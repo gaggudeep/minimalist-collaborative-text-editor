6e4270f43910ded56d87708983c1afa825ef7251
Gagandeep Singh <gagandeepsinghjubbal@gmail.com>
2021-04-02 17:54:39 +0530

    Added font size  and color change functionality

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,19 +3,29 @@ import { Form, Navbar } from "react-bootstrap";
 import "../index.css";
 
 function Header(props) {
-  const handleFontChange = (e) => {
-    document.getElementById("textbox").style.fontSize = `${e.target.value}px`;
+  const handleAttributeChange = (e, att) => {
+    let selectedDom = window.getSelection();
+    let selectedText = selectedDom.toString();
+    let newElement = document.createElement("span");
+    newElement.style =
+      att === "size"
+        ? `font-size: ${e.target.value}px;`
+        : `color: ${e.target.value};`;
+    newElement.innerHTML = selectedText;
+    let range = selectedDom.getRangeAt(0);
+    range.deleteContents();
+    range.insertNode(newElement);
   };
 
   return (
     <>
       <Navbar className="header col-12" sticky="top">
-        <div className="utilities d-flex col-4">
+        <div className="d-flex col-4">
           <Form.Control
             as="select"
             size="sm"
-            onChange={(e) => handleFontChange(e)}
-            className="col-md-2 col-sm-4 font-size-selector"
+            onChange={(e) => handleAttributeChange(e, "size")}
+            className="col-md-2 col-sm-4 utility"
           >
             <option value="12">12</option>
             <option value="14">14</option>
@@ -24,8 +34,14 @@ function Header(props) {
             <option value="20">20</option>
             <option value="24">24</option>
             <option value="28">28</option>
-            <option>Custom value</option>
           </Form.Control>
+          <input
+            type="color"
+            style={{ padding: "0" }}
+            className="col-md-2 col-sm-4 utility"
+            defaultValue="#faebd7"
+            onChange={(e) => handleAttributeChange(e, "color")}
+          />
         </div>
         <Navbar.Brand className="col-4 d-flex justify-content-center">
           Text-Editor
--- src/components/TextBox.js
+++ src/components/TextBox.js
@@ -4,26 +4,34 @@ import io from "socket.io-client";
 
 const socket = io("http://localhost:3001");
 
+// BUG
+/*
+  1. Can't change color and font size simultaneously
+  2. Can't send NodeList thtough socket
+*/
+
+// TODO
 socket.on("text", (text) => {
-  console.log(text);
   localStorage.setItem("text", text);
-  document.getElementById("textbox").value = text;
+  document.getElementById("textbox", text);
 });
 
 function TextBox() {
+  // TODO
   const handleChange = (e) => {
-    localStorage.setItem("text", e.target.value);
-    socket.emit("text", e.target.value);
+    localStorage.setItem("text", e.target.text);
+    socket.emit("text", e.target.text);
   };
 
   return (
     <>
-      <textarea
+      <div
+        contentEditable="true"
         className="textbox"
         id="textbox"
-        onChange={(e) => handleChange(e)}
+        onInput={(e) => handleChange(e)}
         defaultValue={localStorage.getItem("text")}
-      ></textarea>
+      ></div>
     </>
   );
 }
